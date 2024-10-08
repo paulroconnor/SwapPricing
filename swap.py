157676dfeaac41e2c5cadcdd2ee389678469b8cf
paulroconnor <149101926+paulroconnor@users.noreply.github.com>
2024-10-08 17:51:35 -0400

    Add yeartime

--- swap.py
+++ swap.py
@@ -2,6 +2,78 @@ import pandas as pd
 from datetime import datetime
 import ustreasurycurve as ustc
 
+
+def isleapyear(year):
+    if (year % 4 == 0):
+        return True
+    elif (year % 100 == 0) and (year % 400 == 0):
+        return True
+    else:
+        return False
+
+
+def yeartime(end, start, convention):
+
+    d1, m1, y1 = [start.day, start.month, start.year]
+    d2, m2, y2 = [end.day, end.month, end.year]
+
+    if convention == '30/360':
+        return (360 * (y2 - y1) + 30 * (m2 - m1) + (d2 - d1)) / 360
+
+    elif convention == '30U/360':
+        if ((m1 == 2) and (d1 in [28, 29])) and ((m2 == 2) and (d2 in [28, 29])):
+            d2 = 30
+        if (m1 == 2) and (d1 in [28, 29]):
+            d1 = 30
+        if (d2 == 31) and (d1 in [30, 31]):
+            d2 = 30
+        if (d1 == 31):
+            d1 = 30
+        return (360 * (y2 - y1) + 30 * (m2 - m1) + (d2 - d1)) / 360
+
+    elif convention == '30B/360':
+        d1 = min(d1, 30)
+        if d1 > 29:
+            d2 = min(d2, 30)
+        if (d2 == 31) and (d1 in [30, 31]):
+            d2 = 30
+        if (d1 == 31):
+            d1 = 30
+        return (360 * (y2 - y1) + 30 * (m2 - m1) + (d2 - d1)) / 360
+
+    elif convention == '30E/360':
+        if (d1 == 31):
+            d1 = 30
+        if (d2 == 31):
+            d2 = 30
+        return (360 * (y2 - y1) + 30 * (m2 - m1) + (d2 - d1)) / 360
+    
+    elif convention == 'Actual/Actual':
+
+        if isleapyear(y1):
+            dec1 = (start - datetime(y1,12,31)).days / 366
+        else:
+            dec1 = (start - datetime(y1,12,31)).days / 365
+
+        if isleapyear(y2):
+            dec2 = (end - datetime(y2,1,1)).days / 366
+        else:
+            dec2 = (end - datetime(y2,1,1)).days / 365
+        
+        return dec1 + dec2 + len([year for year in range(y1 + 1, y2)])
+
+    elif convention == 'Actual/365':
+        return (end - start).days / 365
+
+    elif convention == 'Actual/360':
+        return (end - start).days / 360
+
+    elif convention == 'Actual/364':
+        return (end - start).days / 364
+    
+
+
+
 class Swap:
 
     def __init__(self, notional, fixed, floating, maturity, frequency, daycount, valuation):
@@ -25,7 +97,3 @@ class Swap:
         ts = ts.iloc[:,1:].melt(var_name = 'tenor', value_name = 'rate')
         ts['time'] = [tenormapping[tenor] for tenor in ts['tenor']]
         return ts[['tenor','time','rate']]
-
-
-
-#   
